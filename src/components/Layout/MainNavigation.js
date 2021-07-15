2904ee41d9c7bc6e066b26f4f94e5050446a7fe4
ritesh-jadhav <jadhav.ritesh0416@gmail.com>
2021-07-16 05:05:13 +0530

    split out reusable component

--- src/components/Layout/MainNavigation.js
+++ src/components/Layout/MainNavigation.js
@@ -1,23 +1,19 @@
 import {React} from "react";
 import { Link } from "react-router-dom";
-import { classes } from "./MainNavigation.module.css";
+import  classes  from "./MainNavigation.module.css";
 
 const MainNavigation = (props) => {
   return (
-    <header>
-      <div>React Meetup</div>
+    <header className={classes.header}>
+      <div className={classes.logo}>React Meetup</div>
       <nav>
         <ul>
           <li>
             <Link to="/">All Meetups</Link>
           </li>
-        </ul>
-        <ul>
           <li>
             <Link to="/new-meetup">New Meetup</Link>
           </li>
-        </ul>
-        <ul>
           <li>
             <Link to="/favourites">Favourites</Link>
           </li>
